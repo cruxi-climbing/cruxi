fc0c9f37157c9c7b4e3d1f7dd8b9bf6bae1768c2
Alexandre Thebault <alexandre.thebault77@gmail.com>
2026-05-27 12:15:24 +0200

    refactor(api): include drizzle schema to drizzle instance

--- apps/api/src/database/index.ts
+++ apps/api/src/database/index.ts
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/bun-sql";
+import * as schema from "./schema";
 
-export const database = drizzle({
-	connection: { url: process.env.DATABASE_URL },
-});
+const connectionUrl = process.env.DATABASE_URL;
+if (!connectionUrl)
+	throw new Error("DATABASE_URL environment variable is not set");
+
+export const database = drizzle(connectionUrl, { schema: schema });
