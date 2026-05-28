3554a088f19f77fa02678963990b5be92ee5baf1
Alexandre Thebault <alexandre.thebault77@gmail.com>
2026-05-28 19:25:20 +0200

    fix(api): :lock: add csrf protection

--- apps/mobile/src/query.ts
+++ apps/mobile/src/query.ts
@@ -1,6 +1,7 @@
 import type { ApiClient } from "@cruxi/api";
 import { createORPCClient } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
+import { SimpleCsrfProtectionLinkPlugin } from "@orpc/client/plugins";
 import { createTanstackQueryUtils } from "@orpc/tanstack-query";
 
 const link = new RPCLink({
@@ -8,6 +9,7 @@ const link = new RPCLink({
 	headers: () => ({
 		authorization: "Bearer token",
 	}),
+	plugins: [new SimpleCsrfProtectionLinkPlugin()],
 });
 
 const client: ApiClient = createORPCClient(link);
