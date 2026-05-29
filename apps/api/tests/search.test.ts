7f24ba2d1303c4ab887314ad1d96b19a327051f4
Alexandre Thebault <alexandre.thebault77@gmail.com>
2026-05-29 16:29:48 +0200

    test(api): add ability to authent in tests

--- apps/api/tests/search.test.ts
+++ apps/api/tests/search.test.ts
@@ -2,15 +2,16 @@ import { describe, expect, it } from "bun:test";
 import { call } from "@orpc/server";
 import { database } from "@/database";
 import { router } from "@/router";
+import { createTestUser } from "./utils/test.utils";
 
 describe("Search", () => {
 	it("should return search results for a valid query", async () => {
+		const { headers } = await createTestUser();
+
 		const res = await call(
 			router.search,
 			{ search: "test" },
-			{
-				context: { headers: new Headers(), database },
-			},
+			{ context: { headers, database } },
 		);
 		expect(res).toBeDefined();
 	});
