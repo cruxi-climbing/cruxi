8893f5815296c2ebd943bff141c0db6a299811cd
Alexandre Thebault <alexandre.thebault77@gmail.com>
2026-05-30 19:25:40 +0200

    refactor(api): :fire: remove auth bypass
    not really useful, better auth handle that better

--- apps/api/src/auth/auth.middleware.ts
+++ apps/api/src/auth/auth.middleware.ts
@@ -7,20 +7,6 @@ export const authMiddleware = baseOrpc.middleware(async ({ context, next }) => {
 		headers: context.headers,
 	});
 
-	const bypassAuth = process.env.BYPASS_AUTH === "true";
-
-	if (bypassAuth) {
-		console.warn(
-			"Bypassing authentication. This should not be used in production.",
-		);
-		return next({
-			context: {
-				session: sessionData?.session,
-				user: sessionData?.user,
-			},
-		});
-	}
-
 	if (!sessionData?.session || !sessionData?.user) {
 		throw new ORPCError("UNAUTHORIZED");
 	}
