665087566e22c82ed173261c23a5168a717a6b65
Holyriss <alexandre.thebault77@gmail.com>
2026-06-05 10:19:24 +0200

    feat(api): :hammer: add reference data script

---
+++ apps/api/src/scripts/add-reference-data.ts
@@ -0,0 +1,11 @@
+import { addReferenceData } from "@/database";
+
+
+try {
+  await addReferenceData()
+  console.info("Reference data successfully added")
+  process.exit(0)
+} catch(e){
+  console.error(e)
+  process.exit(1)
+}
